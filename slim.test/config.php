fdb5936f9ded6c8d91f07e9acb88580e30e3b126
Allan Denis <allancomll@gmail.com>
2016-07-23 03:05:32 -0300

    [Re]Iniciando do zero com Python e TDD. :)

--- slim.test/config.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-	// Evita acesso direto a arquivos incluídos
-	defined('NOPONTO') or die("Acesso direto proibido. ;)");
-	
-	const BD_HOST =		'localhost';
-	const BD_BANCO =		'noponto';
-	const BD_USUARIO =	'noponto';
-	const BD_SENHA =		'ZJEAJ76WyD7SvJZy';
-?>
---
+++ test_banco.py
@@ -0,0 +1,5 @@
+def test_mysql():
+    """Testa se o MySQL e o conector estão instalados"""
+    import mysql.connect
+    print("Fim")
+    return 0
