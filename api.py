d932b9bc43f8c77e514d8bcfed9e6349db82794e
Allan Denis <allancomll@gmail.com>
2016-10-13 02:29:00 -0300

    Carregando veículos de cada viagem. Falta implementar multithreading.

--- api.py
+++ api.py
@@ -1,36 +1,40 @@
 DEBUG = True
-import urllib
-import re
-import xmltodict
+import json, re, urllib, xmltodict
 from pprint import pprint
 from time import time
 
 inicio = time()
-
+API_CITTAMOBI = 'http://api.plataforma.cittati.com.br/m3p/js'
 URL_LINHAS = 'http://info.plataforma.cittati.com.br/m3p/embedded/predictionMap'
 ID_SMTT = 415
 
-def pegaLinhasXml():
-    linhas = urllib.request.urlopen(URL_LINHAS)
-    sessao = re.findall(r'[0-9A-F]{32}', linhas.geturl())[0]
-    linhas = urllib.request.urlopen(URL_LINHAS
-                                    +';jsessionid=%s?0-1.IBehaviorListener.0-mapPanel&out=false&ta=%d'
-                                     % (sessao, ID_SMTT))
-    return str(linhas.read())
 
 
-def linhas():
+def pegaLinhas():
+    def pegaLinhasXml():
+        linhas = urllib.request.urlopen(URL_LINHAS)
+        sessao = re.findall(r'[0-9A-F]{32}', linhas.geturl())[0]
+        linhas = urllib.request.urlopen(URL_LINHAS
+        +';jsessionid=%s?0-1.IBehaviorListener.0-mapPanel&out=false&ta=%d'
+        % (sessao, ID_SMTT))
+        return str(linhas.read().decode('utf8'))
+
     linhasXml = []
+    correcoes = {
+        r'(--)':'',
+        r'\n\t?':'#',
+        r'optgroup':'linha',
+        r'option':'viagens',
+        r'\( +':'(',
+        r'\) +':')',
+        r'( +[xX/] +)':' / ',
+    }
+
     while len(linhasXml) == 0:
         if DEBUG: print("Carregando linhas...")
         linhasXml = pegaLinhasXml()
-        linhasXml = re.sub(r'(--)','',linhasXml)
-        linhasXml = re.sub(r'\n\t?','#',linhasXml)
-        linhasXml = re.sub(r'optgroup','linha',linhasXml)
-        linhasXml = re.sub(r'option','viagens',linhasXml)
-        linhasXml = re.sub(r'\( +','(',linhasXml)
-        linhasXml = re.sub(r'\) +',')',linhasXml)
-        linhasXml = re.sub(r'( +[xX/] +)',' / ',linhasXml)
+        for erro, correcao in correcoes.items():
+            linhasXml = re.sub(erro, correcao, linhasXml)
         filtro = re.compile(r'<linha.*/linha>')
         linhasXml = re.findall(filtro,linhasXml)
     if DEBUG: print("Tratando dados...")
@@ -39,18 +43,52 @@ def linhas():
 
     if DEBUG: print("Extraindo dados...")
     linhas = xmltodict.parse(linhasXml)['xml']['linha']
+    viagens = {}
+    i = 0
+    totalLinhas = len(linhas)
     for linha in linhas:
-        linha['numero'], linha['nome-linha'] = linha.pop('@label').split(' - ', 1)
-        print(len(linha['viagens']), linha['nome-linha'])
+        i += 1
+        linha['numero'] = re.findall(r'\d{2,}', linha['@label'])[0]
+        linha['nome'] = linha.pop('@label')
+        # print(len(linha['viagens']), linha['nome'])
         for viagem in linha['viagens']:
             viagem['id'] = viagem.pop('@value')
-            nome = viagem.pop('#text').split(' - ')
-            viagem['nome-viagem'] = ' '.join(nome[:-1])
-            viagem['direcao'] = nome[-1].lower()
-            print('\t' + viagem['direcao'] + '\t', viagem['nome-viagem'])
+            if linha['numero'] == '797': print(viagem['id'])
+            nome = viagem.pop('#text')
+            viagem['direcao'] = 'volta' if 'VOLTA' in nome else 'ida'
+            viagem.update({
+                'nome' : re.sub(r'(\ *\-\ *)(IDA|VOLTA)', '', nome),
+                'linha' : linha['nome'],
+                'numero' : linha['numero'],})
+            # if DEBUG:
+            #     print("%d/%d\tCarregando veículos:" % (i, totalLinhas),
+            #         viagem['nome'])
+            #         viagem['veiculos'] = pegaVeiculos(viagem['id'])
+            viagens[viagem['id']] = dict(viagem)
 
-    print('%d linhas (%d s)' % (len(linhas), time()-inicio))
+    if DEBUG: print('%d linhas, %d viagens (em %d s)' % (len(linhas), len(viagens), time()-inicio))
+    return viagens
+
+def pegaVeiculos(idViagem):
+    URL_VEICULOS = API_CITTAMOBI + '/vehicles/service/' + str(idViagem)
+    veiculos = urllib.request.urlopen(URL_VEICULOS)
+    veiculos = str(veiculos.read().decode('utf8'))
+    veiculos = json.loads(veiculos)
+    traducoes = {
+        'bearing': 'angulo',
+        'lat': 'lat',
+        'lng': 'lng',
+        'plate': 'placa',
+        'prefix': 'numero',
+        'ts': 'datahora',
+    }
+    for v in veiculos:
+        for original, novo in traducoes.items():
+            v[novo] = v.pop(original)
+    return veiculos
 
 
 if __name__ == '__main__':
-    linhas()
+    linhas = pegaLinhas()
+    pprint(linhas)
+    # pprint(pegaVeiculos(22514))
